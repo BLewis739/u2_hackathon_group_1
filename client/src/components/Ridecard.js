db18bcb59d01a79e49488a21763767ec12174453
Bryan <bryannsanchez1234@gmail.com.com>
2022-03-24 08:47:22 -0700

    updated about me

--- client/src/components/Ridecard.js
+++ client/src/components/Ridecard.js
@@ -1,10 +1,13 @@
 const RideCard = (props) => {
 
   return (
-    <div className='details-summary' onClick={props.onClick}>
+    <div className='ridecard' onClick={props.onClick}>
       <div className='img-wrapper'>
         <img src={props.image} alt='404 err'></img>
 
+      </div>
+      <div className='ridecard-info'>
+        
       </div>
       
     </div>
