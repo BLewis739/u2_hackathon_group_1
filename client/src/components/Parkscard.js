8ecd6da34ae866740999b7bff56d6ec426eaec62
Huan <huan.zeng1@gmail.com>
2022-03-24 14:26:27 -0400

    added background

--- client/src/components/Parkscard.js
+++ client/src/components/Parkscard.js
@@ -3,12 +3,12 @@ import waterPark from '../thrills & spills imgs/waterpark/waterPark.jpg'
 const ParksCard = (props) => {
   return (
     <div className="parkcards" onClick={props.onClick}>
-      <div className="img-wrapper">
-        <img src={props.background_image}></img>
-      </div>
       <div className="parkcards-info">
         <h3>{props.name}</h3>
         <p>{props.rating}</p>
+      </div>
+      <div className="img-wrapper">
+        <img src={props.background_image}></img>
       </div>
     </div>
   )
