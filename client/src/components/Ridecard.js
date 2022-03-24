f401a3e3d402aeb7ee04eb1ca881489a367568d6
Bryan <bryannsanchez1234@gmail.com.com>
2022-03-24 07:57:10 -0700

    components done

--- client/src/components/Ridecard.js
+++ client/src/components/Ridecard.js
@@ -2,6 +2,10 @@ const RideCard = (props) => {
 
   return (
     <div className='details-summary' onClick={props.onClick}>
+      <div className='img-wrapper'>
+        <img src={props.image} alt='404 err'></img>
+
+      </div>
       
     </div>
 
