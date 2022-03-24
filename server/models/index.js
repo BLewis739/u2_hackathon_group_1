de21d6656d9904efe0a2f7cbbb11a1ac4663595c
Brad Lewis <BLewis739@gmail.com>
2022-03-24 10:55:06 -0400

    Set rides as parent with no references

--- server/models/index.js
+++ server/models/index.js
@@ -3,12 +3,12 @@ const ParkSchema = require('./Park')
 const CategorySchema = require('./Category')
 const RideSchema = require('./Ride')
 
-const Park = model('park', ParkSchema)
-const Category = model('categories', CategorySchema)
 const Ride = model('rides', RideSchema)
+const Park = model('parks', ParkSchema)
+const Category = model('categories', CategorySchema)
 
 module.exports = {
+  Ride,
   Park,
-  Category,
-  Ride
+  Category
 }
