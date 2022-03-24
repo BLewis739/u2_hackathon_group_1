41f95b82b5efe07ed257f8ec805dc65975409ec7
Huan <huan.zeng1@gmail.com>
2022-03-24 15:20:42 -0400

    style create ride

--- client/src/components/CreateRide.js
+++ client/src/components/CreateRide.js
@@ -1,44 +1,41 @@
-
-
 const CreateRide = (props) => {
-
   return (
-    
-<form onSubmit={props.onSubmit}>
-  
-      <input
-        type="text"
-        name="Ride"
-        value={props.value}
-        placeholder="Name"
-        onChange={props.onChange}>
-      </input>
-      <input
-        type="text"
-        name="Description"
-        value={props.value}
-        placeholder="Description"
-        onChange={props.onChange}>
-      </input>
-      <input
-        type="text"
-        name="Ride Length"
-        value={props.value}
-        placeholder="Ride Length"
-        onChange={props.onChange}>
-      </input>
-      <input
-        type="text"
-        name="Year Built"
-        value={props.value}
-        placeholder="Year Built"
-        onChange={props.onChange}>
-      </input>
-      <button className="submitButton" text="Submit">
-        Add
-      </button>
-    </form>
+    <div className="createRide">
+      <h1>Add Your Ride</h1>
+      <form onSubmit={props.onSubmit}>
+        <input
+          type="text"
+          name="Ride"
+          value={props.value}
+          placeholder="Name"
+          onChange={props.onChange}
+        ></input>
+        <input
+          type="text"
+          name="Description"
+          value={props.value}
+          placeholder="Description"
+          onChange={props.onChange}
+        ></input>
+        <input
+          type="text"
+          name="Ride Length"
+          value={props.value}
+          placeholder="Ride Length"
+          onChange={props.onChange}
+        ></input>
+        <input
+          type="text"
+          name="Year Built"
+          value={props.value}
+          placeholder="Year Built"
+          onChange={props.onChange}
+        ></input>
+        <button className="submitButton" text="Submit">
+          Add
+        </button>
+      </form>
+    </div>
   )
-
 }
 export default CreateRide
