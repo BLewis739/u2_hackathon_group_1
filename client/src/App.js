379a13b978313971a820d46a0a9eeca7f9bfde53
Huan <huan.zeng1@gmail.com>
2022-03-24 11:11:22 -0400

    add route

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
-import './App.css';
+import './App.css'
 import Home from './pages/Home'
+import About from './pages/About'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-     <header></header>
-     <main>
-       <Routes>
-          <Route path='/' element={<Home />} />
-       </Routes>
-     </main>
-
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="About" element={<About />} />
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
