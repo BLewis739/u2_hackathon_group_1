b2d9babf1ed9ceabafb228693a9d03ee42288e0d
Huan <huan.zeng1@gmail.com>
2022-03-24 13:39:40 -0400

    added style

--- client/src/components/Parkscard.js
+++ client/src/components/Parkscard.js
@@ -3,9 +3,7 @@ import waterPark from '../thrills & spills imgs/waterpark/waterPark.jpg'
 const ParksCard = (props) => {
   return (
     <div className="parkcards" onClick={props.onClick}>
-      <p>park</p>
       <div className="img-wrapper">
-        <title></title>
         <img src={props.background_image}></img>
       </div>
       <div className="parkcards-info">
